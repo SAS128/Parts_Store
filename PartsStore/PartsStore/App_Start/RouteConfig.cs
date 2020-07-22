76ccefc002f0c8297597368223dd712acefcda81
User <User@WIN-PAI2S3FL083>
2020-07-22 17:51:11 +0300

    Add Admin pages

--- PartsStore/PartsStore/App_Start/RouteConfig.cs
+++ PartsStore/PartsStore/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Routing;
+﻿using System.Web.Mvc;
+using System.Web.Routing;
 
 //namespace PartsStore.App_Start
 namespace PartsStore
@@ -16,8 +17,15 @@ namespace PartsStore
 
             routes.MapPageRoute("checkout", "checkout", "~/Pages/Checkout.aspx");
 
-            //routes.MapPageRoute("admin_orders", "admin/orders", "~/Pages/Admin/Orders.aspx");
+            routes.MapPageRoute("admin_orders", "admin/orders", "~/Pages/Admin/Orders.aspx");
             routes.MapPageRoute("admin_details", "admin/details", "~/Pages/Admin/Details.aspx");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
         }
     }
 }
---
+++ PartsStore/PartsStore/Pages/Admin/Details.aspx.cs
@@ -0,0 +1,51 @@
+﻿using PartsStore.Models;
+using PartsStore.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PartsStore.Pages.Admin
+{
+    public partial class Details : System.Web.UI.Page
+    {
+        private Repository repository = new Repository();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IEnumerable<Detail> GetDetails() => repository.Details;
+
+
+        public void UpdateGames(int gameID)
+        {
+            Detail myGame = repository.Details.Where(p => p.DetailsId == gameID).FirstOrDefault();
+            if (myGame != null && TryUpdateModel(myGame, new FormValueProvider(ModelBindingExecutionContext)))
+            {
+                repository.SaveDetails(myGame);
+            }
+        }
+
+        public void DeleteGames(int gameID)
+        {
+            Detail myGetDetail = repository.Details.Where(p => p.DetailsId == gameID).FirstOrDefault();
+            if (myGetDetail != null)
+            {
+                repository.DeleteDetail(myGetDetail);
+            }
+        }
+        public void InsertGames()
+        {
+            Detail game = new Detail();
+            if (TryUpdateModel(game, new FormValueProvider(ModelBindingExecutionContext)))
+            {
+                repository.SaveDetails(game);
+            }
+        }
+
+    }
+}
